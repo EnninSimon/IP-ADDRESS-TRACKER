a4827e36764e4e0d7c86622ac5e40dcf9fd31c2f
Ennin Simon <enninsimon@gmail.com>
2021-10-23 17:56:55 +0100

    IP Address Tracker 2

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+
+try {
+  document.getElementById('submit').onclick = async function getIpAddress() {
+    var input = document.getElementById('mybar').value.toString();
+    var url = 'https://geo.ipify.org/api/v2/country?';
+    var apikey = 'apiKey=at_XGn4k6pJquxPqauEaqQYMZEoVhkKI&ipAddress=';
+    // var apiAddress = '102.176.65.232';
+    var apiUrl = url + apikey + input;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data)
+
+    //Seting the api in the ui
+    const { ip, isp } = data;
+    document.getElementById('ipAddress').textContent = ip;
+    document.getElementById('isp').textContent = isp;
+    document.getElementById('country').textContent = data.location.country;
+    document.getElementById('region').textContent = data.location.region;
+    document.getElementById('timezone').textContent = data.location.timezone;
+  }
+
+
+} catch (error) {
+  console.log(error)
+}
+
+
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiZW5uaW4tc2ltb24iLCJhIjoiY2tqdjNycXlzMDRzNjJwbDdzN20xZGwxeCJ9.4iQpygdTEx3qnOuwmxeMJg';
 var map = new mapboxgl.Map({
   container: 'map',
@@ -26,33 +55,8 @@ map.addControl(new mapboxgl.GeolocateControl({
   showUserHeading: true
 }));
 
-map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector('body')}));
+map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body') }));
 
 //-----------------------------------------//
 
-//Add api
-const apiUrl = 'https://geo.ipify.org/api/v2/country?apiKey=at_XGn4k6pJquxPqauEaqQYMZEoVhkKI&ipAddress=8.8.8.8'
-async function getIpAddress() {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  console.log(data)
-
-
-  //Seting the api in the ui
-  const { ip, isp } = data;
-  document.getElementById('ipAddress').textContent = ip;
-  document.getElementById('isp').textContent = isp;
-  document.getElementById('country').textContent = data.location.country;
-  document.getElementById('region').textContent = data.location.region;
-  document.getElementById('timezone').textContent = data.location.timezone;
-
-}
-getIpAddress();
-
-// fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_XGn4k6pJquxPqauEaqQYMZEoVhkKI&ipAddress=8.8.8.8`)
-//   .then(res => res.json())
-//   .then(
-//     data => console.log(data)
-//   )
-//   .catch(error => console.log(error))
 
